0dc7417aadd919f3f434d46d059a87c39518347c
jolee <jolee@adobe.com>
2017-03-27 13:48:57 -0700

    update

--- video_compare.py
+++ video_compare.py
@@ -1,7 +1,10 @@
 import cv2
 import glob
+import sys
 
-dirpath = 'vggm'
+dirpath = 'vgg16'
+if len(sys.argv) > 1:
+	dirpath = sys.argv[1]
 
 files = glob.glob(dirpath + '_sdsel/*.txt')
 
@@ -16,6 +19,7 @@ for fname in files:
 	vid_name2 = vid_result2[0].split()[1]
 	gt_label2 = vid_result2[1].split()[1]
 	frame_pred2 = vid_result2[2:]
+	frame_pred2 = vid_result2[:len(frame_pred)]
 
 	if vid_name != vid_name2:
 		print('Video names are not equal!')
@@ -27,6 +31,15 @@ for fname in files:
 		print(fname, vid_name, fname2, vid_name2)
 		continue
 
+	nFalseS = sum([gt_label != pred for pred in frame_pred2])
+	nFalseSDSEL = sum([gt_label != pred for pred in frame_pred])
+
+	if nFalseSDSEL*2 > len(frame_pred):
+		print('Too bad result- False Num: {} among {}.'.format(nFalseSDSEL, len(frame_pred)))
+		continue
+	if nFalseS < nFalseSDSEL*2:
+		print('Too similar results - False Num: {} vs {}.'.format(nFalseS, nFalseSDSEL))
+		continue
 
 	fourcc = cv2.VideoWriter_fourcc(*'XVID')
 	font = cv2.FONT_HERSHEY_SIMPLEX
@@ -39,13 +52,13 @@ for fname in files:
 
 		cv2.putText(img, 'GT: {}'.format(gt_label), (10,20), font, 0.5, (255,255,255), 1, ln)
 		if gt_label == frame_pred2[ind]:
-			color = (255,255,255)
+			color = (255,0,0)
 		else:
 			color = (0,0,255)
 		cv2.putText(img, ' S: {}'.format(frame_pred2[ind]), (10,40), font, 0.5, color, 1, ln)
 
 		if gt_label == frame_pred[ind]:
-			color = (255,255,255)
+			color = (255,0,0)
 		else:
 			color = (0,0,255)
 		cv2.putText(img, ' M: {}'.format(frame_pred[ind]), (10,60), font, 0.5, color, 1, ln)
